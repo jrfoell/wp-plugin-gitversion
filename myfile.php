830c07323753bd3cb727c339b388aad5c604401e
Justin Foell <justin_f@cimbura.com>
2016-04-19 11:40:25 -0500

    Latest version

--- myfile.php
+++ myfile.php
@@ -1,6 +1,11 @@
 <?php
 
-define( 'VERSION', '$Id$' );
+define( 'PLUGIN_DIR', dirname( __FILE__ ) );
+
+function get_git_version() {
+	return exec('cd '. PLUGIN_DIR.'; git describe');
+}
 
 // Comment 3
-echo "current version is: " . VERSION;
+echo "file is in: " . PLUGIN_DIR . PHP_EOL;
+echo "current version is: " . get_git_version() . PHP_EOL;

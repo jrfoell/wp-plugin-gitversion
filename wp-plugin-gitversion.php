b867f3d0c8687f86d32c693e521ed25700e059da
Justin Foell <justin_f@cimbura.com>
2016-04-19 13:27:12 -0500

    Added some documentation

--- wp-plugin-gitversion.php
+++ wp-plugin-gitversion.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Git Version
+Plugin Name: WP Plugin Git Version
 Description: Override a plugin version with a more accurate git version using `git describe`
 Version:     1.1
 */
@@ -10,11 +10,18 @@ define( 'PLUGIN_DIR', basename( PLUGIN_PATH ) );
 define( 'PLUGIN_FILE', basename( __FILE__) );
 
 /**
- * `git describe` assumes you have tagged your project with a "annotated tag" which means a tag with a comment such as:
+ * This plugin uses `git describe` to override the plugin version
+ * shown on the plugin page: wp-admin/plugins.php
+ *
+ * The plugin version is only overridden when the plugin is active.
+ *
+ * `git describe` assumes you have tagged your plugin with an
+ * "annotated tag" which means a tag with a comment such as:
  *
  * `git tag -a 1.1 -m "version 1.1"`
  *
- * Once an annotated tag is added, `git describe` will give you the tag name with some extra info such as:
+ * Once an annotated tag is added, `git describe` will give you the
+ * tag name with some extra info such as:
  *
  * 1.1-3-g7142af9
  *
@@ -24,7 +31,7 @@ define( 'PLUGIN_FILE', basename( __FILE__) );
  */
 
 function git_describe() {
-	return exec('cd '. PLUGIN_PATH.'; git describe');
+	return exec( 'cd '. PLUGIN_PATH.'; git describe' );
 }
 
 function git_version( $all_plugins ) {
